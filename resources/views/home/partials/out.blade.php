e6cc4874c82018b318dc47642edc396b0f2d00e9
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-12-07 13:38:07 +0100

    Added @media mixin

--- resources/views/home/partials/out.blade.php
+++ resources/views/home/partials/out.blade.php
@@ -1,7 +1,7 @@
 @foreach($out as $o) 
 <div class="child-card flex">
   <div class="flex column btn">
-    <i class="fa fa-sign-in" id="{{$o->child_id}}"></i>
+    <span class="padded"></span> 
     <span class="padded"></span> 
     <i class="fa fa-pencil" id="{{$o->child_id}}"></i>
   </div>
