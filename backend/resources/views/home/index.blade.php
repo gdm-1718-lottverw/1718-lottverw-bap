cdafe1b2060d2ed7e3c654b4bffded3bc0cd2ad8
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-12-07 13:38:07 +0100

    Added @media mixin

--- backend/resources/views/home/index.blade.php
+++ backend/resources/views/home/index.blade.php
@@ -4,13 +4,13 @@
 
   <section class="home" id="home">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <div id="to-come" class="flex">
+    <div id="to-come" class="flex wrap justified-start">
       @include('home.partials.future')
     </div>  
-    <div id="in" class="flex">
+    <div id="in" class="flex wrap justified-start">
       @include('home.partials.in')
     </div>
-    <div id="out" class="flex">
+    <div id="out" class="flex wrap justified-start">
       @include('home.partials.out')
     </div> 
   </section>
--- backend/resources/views/home/partials/in.blade.php
+++ backend/resources/views/home/partials/in.blade.php
@@ -1,8 +1,9 @@
 @foreach($in as $i) 
 <div class="child-card flex">
   <div class="flex column btn">
-    <i class="fa fa-sign-in" id="{{$i->child_id}}"></i>
+    
     <span class="padded"></span> 
+    <i class="fa fa-sign-out" id="{{$i->child_id}}"></i>
     <i class="fa fa-pencil" id="{{$i->child_id}}"></i>
   </div>
   <div class="child-info">
