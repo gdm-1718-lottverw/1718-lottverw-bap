35efb63c62be78af683e62654c922a7b59c54931
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-12-08 12:40:06 +0100

    Added responsive styling for home page and navigation.

--- backend/resources/views/home/partials/out.blade.php
+++ backend/resources/views/home/partials/out.blade.php
@@ -1,13 +1,12 @@
 <div id="out"  class="flex wrap justified-start">
   @foreach($out as $o) 
     <div class="child-card flex">
-      <div class="flex column btn">
-        <span class="padded"></span> 
-        <span class="padded"></span> 
+      <div class="flex column btn justified-end">
         <i class="fa fa-pencil" id="{{$o->child_id}}"></i>
       </div>
       <div class="child-info flex column">
         <span class="name">{{$o->name}}</span>
+        <p class="description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
       </div>
     </div>  
   @endforeach
