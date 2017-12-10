82088a2fe808cea66c4114773c3720a25331b815
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-12-10 13:18:02 +0100

    Some button styling also added a link to log from home.

--- backend/resources/views/home/partials/future.blade.php
+++ backend/resources/views/home/partials/future.blade.php
@@ -3,7 +3,7 @@
   <div class="child-card flex">
     <div class="flex column btn justified-s">
       <i class="fa fa-sign-in" id="{{$tc->child_id}}"></i>
-      <i class="fa fa-pencil" id="{{$tc->child_id}}"></i>
+      <a href="/log"><i class="fa fa-pencil" id="{{$tc->child_id}}"></i></a>
     </div>
     <div class="child-info flex column">
       <span class="name">{{$tc->name}}</span>
