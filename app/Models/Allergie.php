5ec08f80782a9ff77a45674fb6df76dc7dea4140
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-12-25 11:30:13 +0100

    Updated filter. Working: presence, type, allergies and general (date, or).

--- app/Models/Allergie.php
+++ app/Models/Allergie.php
@@ -12,24 +12,4 @@ class Allergie extends Model
      * @var string
      */
     protected $table = 'allergies';
-
-     /**
-     * Scope a query to only include users of a given type.
-     *
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $date
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeAllergie($query, $conditions)
-    {
-        foreach($conditions as $column => $value)
-        {
-            if(is_array($value)){
-                 foreach($value as $val){
-                    $query->orWhere('type', '=', $val);
-                }
-            }
-           
-        }
-    }
 }
--- app/Models/PlannedAttendance.php
+++ app/Models/PlannedAttendance.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+
 class PlannedAttendance extends Model
 {
     /**
@@ -38,103 +39,6 @@ class PlannedAttendance extends Model
     public function organization()
     {
         return $this->belongsTo('App\Models\Organization', 'organization_id');
-    }
-    
-    /**
-     * Scope a query to only include users of a given type.
-     *
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $date
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopePresent($query, $presence)
-    {
-        switch($presence){
-            case 'present_registered':
-                 $query->where([
-                    ['in', '=', false],
-                    ['out', '=', false]
-                ]);
-                break;
-            case 'present_present':
-                 $query->where([
-                    ['in', '=', true],
-                    ['out', '=', false]
-                ]);
-                break;
-            default:
-                break;
-        }
-        
-    }
-      /**
-     * Scope a query to only include users of a given type.
-     *
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $date
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeGeneral($query, $conditions)
-    {
-        foreach($conditions as $column => $value)
-        {
-            $query->where($column, '=', $value);
-            
-        }
-    }
-    
-    /**
-     * Scope a query to only include users of a given type.
-     *
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $date
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeType($query, $conditions)
-    {
-        foreach($conditions as $column => $value)
-        {
-            if(is_array($value)){
-                foreach($value as $val){
-                    $query->orWhere($column, '=', $val);
-                }
-            }
-        }
-    }
-     /**
-     * Scope a query to only include users of a given type.
-     *
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param mixed $date
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeTest($query, $conditions)
-    {
-        foreach($conditions as $column => $value)
-        {
-            $query->whereHas('child', function ($sub) use($value){
-                if(Count($value) > 1){
-                    for($i = 0; Count($value) > $i ; $i++){
-                        $val = $value[$i];
-                        echo ' oh y ' . $val;
-                        $max = Carbon::now()->subYear((int)$val)->format('Y');
-                        $min = Carbon::now()->subYear(((int)$val) + 2)->format('Y');
-                        $sub->orhere([
-                            ['date_of_birth', '>', $min], 
-                            ['date_of_birth', '<', $max], 
-                        ]);
-                    }
-                } else {
-                    echo 'oh no ' . $value[0];
-                    $max = Carbon::now()->subYear((int)$value[0])->format('Y');
-                    $min = Carbon::now()->subYear(((int)$value[0]) + 2)->format('Y');
-                    $sub->where([
-                       ['date_of_birth', '>', $min], 
-                       ['date_of_birth', '<', $max], 
-                   ]);
-                }
-            });
-        }
     }
 
 }
