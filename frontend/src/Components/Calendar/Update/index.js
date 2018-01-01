fb21e3facf0224402915cbf55d72ae696ec56bcc
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2018-01-01 13:17:03 +0100

    First 2018 commit. :)

--- frontend/src/Components/Calendar/Update/index.js
+++ frontend/src/Components/Calendar/Update/index.js
@@ -16,8 +16,7 @@ class UpdateCalendarService extends Component {
       icon: [],
       children: [],
       child: '', 
-      new_date:'',
-      new_child:'', 
+      new_child: '', 
       new_parent_note:'', 
       new_go_home_alone: '',
       new_type: '',
@@ -32,13 +31,10 @@ class UpdateCalendarService extends Component {
    if (nextProps.item != null && nextProps.children.length > 0 && nextProps.error == undefined) {
       this.state.item = nextProps.item;
       this.state.children = nextProps.children;
-      console.log(nextProps.item, nextProps.children);
-      this.state.children.forEach((child) =>{
-        console.log(child);
-        if(child.id == this.state.item.child_id){
-          this.state.child = child.name;
-        }
-      } )
+      // give the new state the old value for placeholder.
+      this.new_parent_note = this.state.item.parent_note;
+      this.getChild(this.state.children, this.state.item.child_id)
+
       switch(nextProps.item.type){
         case 'morning':
          return this.state.icon.push({name: 'sun-o'});
@@ -51,6 +47,31 @@ class UpdateCalendarService extends Component {
          break;
       }
     } 
+  }
+
+  submit = () => {
+    const loop = [ this.state.new_child, this.state.new_go_home_alone, this.state.new_parent_note, this.state.new_type];
+    let update = {};
+    loop.forEach((item) => {
+      console.log(item);
+      if (item !== undefined && item != null){
+        for(key in this.state){
+          update[key.slice(4)] = item;
+
+        }
+        console.log('UPDATE', update, this.props);
+      }
+
+    })
+  }
+
+  getChild = (children, id) => {
+    children.forEach((child) => {
+      if(child.id == id){
+        this.state.child = child.name;
+        this.state.new_child = child.id;
+      }
+    })
   }
 
   generateIcons = (icons) => {
@@ -71,10 +92,6 @@ class UpdateCalendarService extends Component {
           {this.generateIcon('calendar')}
           <View style={styles.description}>
             <Text style={styles.label}>Ingeschreven op: {this.state.item.date}</Text>
-            <TextInput
-              style={styles.textInput}
-              onChangeText={(date) => {this.setState({'new_date': date})}}
-              value={this.state.item.date} />
           </View>              
         </View>
         <View style={styles.item}>
@@ -82,7 +99,7 @@ class UpdateCalendarService extends Component {
           <View style={styles.description}>
             <Text style={styles.label}>{this.state.child}</Text>
             {this.state.children.map((child, i) => {
-              return (<TouchableOpacity key={i} onPress={() => {this.setState({'new_child': child.id})}}>
+              return (<TouchableOpacity key={i} onPress={() => {this.setState({'new_child': child.id}), this.getChild(this.state.children, child.id)}}>
                   <Text>{child.name}</Text>
                 </TouchableOpacity>)
             })}
@@ -102,7 +119,7 @@ class UpdateCalendarService extends Component {
         <View style={styles.item}>
            {this.generateIcon('bicycle')}
           <View style={styles.description}>
-            <Text style={styles.label}>{this.state.item.go_home_alone == true? 'Kind mag alleen naar huis': 'kind wordt opgehaald'}</Text>
+            <Text style={styles.label}>{this.new_go_home_alone == true? 'Kind mag alleen naar huis': 'kind wordt opgehaald'}</Text>
           </View>              
         </View>
         <View style={styles.item}>
@@ -111,12 +128,14 @@ class UpdateCalendarService extends Component {
             <Text style={styles.label}>Opmerking:</Text>
               <TextInput
                 style={styles.textInput}
-                onChangeText={() => {}}
-                value={this.state.item.parent_notes}/>
+                onChangeText={(note) => {this.setState({new_parent_note: note})}}
+                value={this.state.new_parent_note}/>
           </View>              
         </View>
-      
-      </View>
+        <TouchableOpacity style={styles.btn} onPress={() => {this.submit()}}>
+          <Text style={styles.btnText}>BEWAREN</Text>
+        </TouchableOpacity>     
+     </View>
     );
   }
 }
