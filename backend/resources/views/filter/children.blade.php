47a20c67a18918683426e83277ebec46f7a5c670
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2018-01-13 15:16:11 +0100

    added filter styling

--- backend/resources/views/filter/children.blade.php
+++ backend/resources/views/filter/children.blade.php
@@ -1,12 +1,13 @@
-<div class="filter-results">
+<div class="filter-results flex wrap">
     @foreach($children as $child)
-        <div class="filter-results-item">
-            <span class="child-name">{{$child->child_id}} {{$child->name}}</span>
-            <span class="child-care">
+       <div class="filter-results-item flex column justified-start">
+            <span class="child-name">{{$child->name}}</span>
+            <span class="child-care"></span>
+            <div class="icons flex row flex-child end">
                 @if($child->potty_trained == false)
-                    <i class="fa fa-tint" aria-hidden="true"></i>
+                   <span class="icon flex justified-c centered">  <i class="fa fa-tint" aria-hidden="true"></i></span>
                 @endif
-            </span>
+            </div>
         </div>
     @endforeach
 </div>
--- backend/resources/views/filter/index.blade.php
+++ backend/resources/views/filter/index.blade.php
@@ -1,52 +1,55 @@
 @extends('layouts.app')
 @section('content')
  <section class="filter flex column">
-    <div class="filter-topbar">
+    <div class="filter-topbar flex row justified-start centered">
         {{ Form::open(array('class' => 'flex row', 'action' => 'Backoffice\Filter\IndexController@create')) }}
-        <span class="item"> {{ Form::input('date', 'date', $value = \Carbon\Carbon::now()->format('Y-m-d'), $options = array('class'=>'date')) }}</span>
-        <span class="item"> {{Form::checkbox('birthday', 'true')}} {{ Form::label('birthday', 'Deze maand jarig')}} </span>
-        <span class="item"> {{Form::checkbox('picture', 'true')}} {{ Form::label('picture', 'Geen foto\'s')}} </span>
-        <span class="item"> {{Form::checkbox('potty_trained', 'false')}} {{ Form::label('potty_trained', 'Zindelijkheidstraining')}} </span>
+        <span class="item flex row centered jutified-c"> {{ Form::input('date', 'date', $value = \Carbon\Carbon::now()->format('Y-m-d'), $options = array('class'=>'date')) }}</span>
+        <span class="item flex row centered jutified-c"> {{Form::checkbox('birthday', 'true')}} {{ Form::label('birthday', 'Deze maand jarig')}} </span>
+        <span class="item flex row centered jutified-c"> {{Form::checkbox('picture', 'true')}} {{ Form::label('picture', 'Geen foto\'s')}} </span>
+        <span class="item flex row centered jutified-c"> {{Form::checkbox('potty_trained', 'false')}} {{ Form::label('potty_trained', 'Zindelijkheidstraining')}} </span>
         {{ Form::close() }}
-    </div> 
-    <div class="content">
-        <div class="">
+    </div>  
+    <div class="search-content flex row justified-start">
+        <div class="search">
            {{ Form::open(array('class' => 'flex column', 'action' => 'Backoffice\Filter\IndexController@create')) }}
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="part flex column">
                     <h4 class="sub-title">Aanwezig</h4>
-                    <span class="item"> {{Form::radio('present', 'present_present', true, ['class' => 'checko'])}} {{ Form::label('present', 'Aanwezig')}} </span>
-                    <span class="item"> {{Form::radio('present', 'present_out', null)}} {{ Form::label('out', 'Uit')}} </span>
-                    <span class="item"> {{Form::radio('present', 'present_registered', null, ['class' => 'checko'])}} {{ Form::label('registered', 'Reservatie')}} </span>
-                    <span class="item"> {{Form::radio('present', 'present_all', null)}} {{ Form::label('all', 'alle')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::radio('present', 'present_present', true, ['class' => 'checko'])}} {{ Form::label('present', 'Aanwezig')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::radio('present', 'present_out', null)}} {{ Form::label('out', 'Uit')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::radio('present', 'present_registered', null, ['class' => 'checko'])}} {{ Form::label('registered', 'Reservatie')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::radio('present', 'present_all', null)}} {{ Form::label('all', 'alle')}} </span>
                 </div>
                  <div class="part flex column">
                     <h4 class="sub-title">Opvang type</h4>
-                    <span class="item"> {{Form::checkbox('type', 'voormiddag', null, ['class' => 'checko'])}} {{ Form::label('type', 'Voormiddag')}} </span>
-                    <span class="item"> {{Form::checkbox('type', 'namiddag', null, ['class' => 'checko'])}} {{ Form::label('type', 'Namiddag')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::checkbox('type', 'voormiddag', null, ['class' => 'checko'])}} {{ Form::label('type', 'Voormiddag')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::checkbox('type', 'namiddag', null, ['class' => 'checko'])}} {{ Form::label('type', 'Namiddag')}} </span>
+                </div>
+               <div class="part flex column">
+                    <h4 class="sub-title">Allergie</h4>
+                    <span class="item flex row centered jutified-c"> {{Form::checkbox('allergie', 'food')}} {{ Form::label('allergie', 'Voedsel')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::checkbox('allergie', 'insects')}} {{ Form::label('allergie', 'Insecten')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::checkbox('allergie', 'animals')}} {{ Form::label('allergie', 'Dieren')}} </span>
+                    <span class="item flex row centered jutified-c"> {{Form::checkbox('allergie', 'other')}} {{ Form::label('allergie', 'Ander')}} </span>
                 </div>
                 <div class="part flex column">
                     <h4 class="sub-title">Leeftijd</h4>
                     <div id='slider'></div>
                 </div>
-               <div class="part flex column">
-                    <h4 class="sub-title">Allergie</h4>
-                    <span class="item"> {{Form::checkbox('allergie', 'food')}} {{ Form::label('allergie', 'Voedsel')}} </span>
-                    <span class="item"> {{Form::checkbox('allergie', 'insects')}} {{ Form::label('allergie', 'Insecten')}} </span>
-                    <span class="item"> {{Form::checkbox('allergie', 'animals')}} {{ Form::label('allergie', 'Dieren')}} </span>
-                    <span class="item"> {{Form::checkbox('allergie', 'other')}} {{ Form::label('allergie', 'Ander')}} </span>
-                </div>
             {{ Form::close() }}
         </div>
-        <div class="filter-results">
+        <div class="filter-results flex wrap">
         @foreach($planned as $child)
-            <div class="filter-results-item">
-                <span class="child-name">{{$child->child_id}} {{$child->name}}</span>
+            <div class="filter-results-item flex column justified-start">
+                <span class="child-name">{{$child->name}}</span>
                 <span class="child-care">
-                    @if($child->potty_trained == false)
-                        <i class="fa fa-tint" aria-hidden="true"></i>
-                    @endif
+
                 </span>
+                <div class="icons flex row flex-child end">
+                    @if($child->potty_trained == false)
+                       <span class="icon flex justified-c centered">  <i class="fa fa-tint" aria-hidden="true"></i></span>
+                    @endif
+                </div>
             </div>
         @endforeach
         </div>
