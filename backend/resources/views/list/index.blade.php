cf43f6c97071b3ecdfa2967573d9e1f1795cd907
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2018-01-15 19:11:13 +0100

    Added page title

--- backend/resources/views/list/index.blade.php
+++ backend/resources/views/list/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
   <section class="list">
-  <div class="flex wrap justified-start">
-    @foreach($children as $child)
-      <a href='/child/{{$child->id}}' class="child-item">{{$child->id}}. {{$child->name}}</a>
-    @endforeach
+	<p class="title">Lijst van alle kinderen geregistreerd in deze opvang.</p>
+	<div class="flex wrap justified-start children">
+	   @foreach($children as $child)
+	      <a href='/child/{{$child->id}}' class="child-item">{{$child->id}}. {{$child->name}}</a>
+	    @endforeach
     </div>
   </section>
 @endsection
